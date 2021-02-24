5384b504f8c3d25dfe2511e273c96f2586db78cb
Daniel Lysak <daniel_kn@live.no>
2021-02-24 14:23:57 +0100

    updated design, added shell

---
+++ index.js
@@ -0,0 +1,96 @@
+class Todo {
+    constructor(title, toDoBody, tag, id) {
+        this.title = title;
+        this.toDoBody = toDoBody;
+        this.tag = tag;
+        this.id = id;
+
+    }
+    outputTodo() {
+        if (this.id % 2 == 0) {
+            return `
+    <div id="toDos${this.id}" class="columns is-vcentered">
+      <div class="column">
+        <p class="notification has-text-centered is-info" style="padding: 55px;">Progress bar
+          <br>
+          <br>
+          <progress class="progress is-large is-danger" max="100">30%</progress>
+        </p>
+
+      </div>
+      <div class="column is-two-thirds">
+        <article class="message is-info is-large">
+          <div class="message-header is-size-4">
+            <span class="tag is-dark is-rouded">${this.tag}</span>
+            ${this.title}
+            <button class="button delete has-addon is-black is-large"></button>
+            </div>
+            <div class="message-body">
+              <span class="content is-small">${this.toDoBody}</span>
+              <br>
+              <br>
+              <br>
+              <button class="button is-focused is-info">Update card</button>
+            </div>              
+      </div>
+    </div>
+      `;
+        } else {
+            return `
+    <div id="toDos${this.id}" class="columns is-vcentered">
+      <div class="column is-two-thirds">
+        <article class="message is-info is-large">
+          <div class="message-header is-size-4">
+            <span class="tag is-dark is-rouded">${this.tag}</span>
+            ${this.title}
+            <button class="button delete has-addon is-black is-large"></button>
+            </div>
+            <div class="message-body">
+              <span class="content is-small">${this.toDoBody}</span>
+              <br>
+              <br>
+              <br>
+              <button class="button is-focused is-info">Update card</button>
+            </div>              
+          </div>
+
+      <div class="column">
+        <div class="column">
+          <p class="notification has-text-centered is-info" style="padding: 54px; width: 105%; right: 3%">Progress bar
+            <br>
+            <br>
+            <progress style="width: 100%;" class="progress is-link is-large" value="30" max="100">50%</progress>
+          </p>
+        </div>
+      </div>
+    </div> 
+  </div>
+            `
+        }
+    }
+}
+
+const createBtn = document.getElementById("toDoBtn");
+
+
+
+
+createBtn.onclick = () => createTodos();
+
+
+
+function createTodos() {
+    const toDoBody = document.getElementById("toDosBody").value;
+    const toDoTag = document.getElementById("toDosTag").value;
+    const toDoTitle = document.getElementById("toDosTitle").value;
+    const nodes = document.getElementById('toDos').childElementCount + 1;
+    const primaryDiv = document.getElementById('toDos');
+
+    let newTodo = new Todo(`${toDoTitle}`, `${toDoBody}`, `${toDoTag}`, `${nodes}`);
+    primaryDiv.innerHTML += newTodo.outputTodo();
+
+
+
+
+}
+
